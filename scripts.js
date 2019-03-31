422018f86789b18d5be69f226e634bf94671de21
NateHiser <nate.hiser@gmail.com>
2019-03-31 00:15:39 -0400

    Solved array problem

--- scripts.js
+++ scripts.js
@@ -6,14 +6,18 @@ function openFilter() {
 function closeFilter() {
   document.getElementById("myBtnContainer").style.height = "0px";
 }
+$(document).ready(function() {
 
 $(function(){
-  $.get("data/videos.json", function(videos){
+  $.get("data/videos.json", function(videos) {
+      console.log("hello world");
     $.get("tpl/video.tpl.html", function(source){
+        console.log("hello world");
       var template = Handlebars.compile(source);
       var html = template(videos);
-      console.log(html);
+      console.log("hello world");
       $(".container").append(html);
     });
   });
+});
 });
