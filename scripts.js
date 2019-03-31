42ff8d5ee80ba776978b3f6d28e6b5eafd3196d2
NateHiser <nate.hiser@gmail.com>
2019-03-31 15:23:23 -0400

    upload content to array

--- scripts.js
+++ scripts.js
@@ -10,12 +10,9 @@ $(document).ready(function() {
 
 $(function(){
   $.get("data/videos.json", function(videos) {
-      console.log("hello world");
     $.get("tpl/video.tpl.html", function(source){
-        console.log("hello world");
       var template = Handlebars.compile(source);
       var html = template(videos);
-      console.log("hello world");
       $(".container").append(html);
     });
   });
