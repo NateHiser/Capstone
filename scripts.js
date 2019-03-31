ffb955670c443c38a5b515f981b2aae51639fec4
NateHiser <nate.hiser@gmail.com>
2019-03-30 23:54:33 -0400

    saving progress. Wont load array.

--- scripts.js
+++ scripts.js
@@ -1,3 +1,4 @@
+
 function openFilter() {
   document.getElementById("myBtnContainer").style.height = "300px";
 }
@@ -6,46 +7,13 @@ function closeFilter() {
   document.getElementById("myBtnContainer").style.height = "0px";
 }
 
-filterSelection("all")
-
-function filterSelection(c) {
-  var x, i;
-  x = document.getElementsByClassName("filterDiv");
-  if (c == "all") c = "";
-  for (i = 0; i < x.length; i++) {
-    w3RemoveClass(x[i], "show");
-    if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
-  }
-}
-
-function w3AddClass(element, name) {
-  var i, arr1, arr2;
-  arr1 = element.className.split(" ");
-  arr2 = name.split(" ");
-  for (i = 0; i < arr2.length; i++) {
-    if (arr1.indexOf(arr2[i]) == -1) {element.className += " " + arr2[i];}
-  }
-}
-
-function w3RemoveClass(element, name) {
-  var i, arr1, arr2;
-  arr1 = element.className.split(" ");
-  arr2 = name.split(" ");
-  for (i = 0; i < arr2.length; i++) {
-    while (arr1.indexOf(arr2[i]) > -1) {
-      arr1.splice(arr1.indexOf(arr2[i]), 1);
-    }
-  }
-  element.className = arr1.join(" ");
-}
-
-var btnContainer = document.getElementById("myBtnContainer");
-console.log(btnContainer)
-var btns = btnContainer.getElementsByClassName("btn");
-for (var i = 0; i < btns.length; i++) {
-  btns[i].addEventListener("click", function(){
-    var current = document.getElementsByClassName("active");
-    current[0].className = current[0].className.replace(" active", "");
-    this.className += " active";
+$(function(){
+  $.get("data/videos.json", function(videos){
+    $.get("tpl/video.tpl.html", function(source){
+      var template = Handlebars.compile(source);
+      var html = template(videos);
+      console.log(html);
+      $(".container").append(html);
+    });
   });
-}
+});
